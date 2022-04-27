003385a9ecc2f167a4d1e427e318b2ecdce56aca
Vinh <Vinho0oNguyen@gmail.com>
2022-04-27 11:32:14 +0700

    test pipline3

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -9,6 +9,6 @@ public class HelloWorld implements SparkApplication {
 
 	@Override
 	public void init() {
-		get("/hello", (req, res) -> "Test java123 maven and jenkins with pipeline2");
+		get("/hello", (req, res) -> "Test java123 maven and jenkins with pipeline3");
 	}
 }
